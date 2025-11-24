a135338d03a6005e3bcc90b1043792c46b5d334b
RMSE233 <Sequeirarm@uamv.edu.ni>
2025-11-24 09:56:50 -0600

    Se agrego el codigo de busqueda lineal

--- BusquedaLineal/Program.cs
+++ BusquedaLineal/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusquedaLineal
 {
@@ -10,6 +6,48 @@ namespace BusquedaLineal
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int[] arreglo = new int[20];
+
+            // Generar arreglo con números aleatorios
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                arreglo[i] = rnd.Next(1, 101); // Números aleatorios entre 1 y 100
+            }
+
+            Console.WriteLine("Arreglo generado:");
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write(arreglo[i] + " ");
+            }
+
+            // Solicitar número al usuario
+            Console.WriteLine("\n\nIngrese un número a buscar:");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Búsqueda lineal
+            bool encontrado = false;
+            int posicion = -1;
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == numero)
+                {
+                    encontrado = true;
+                    posicion = i;
+                    break; // Se detiene al encontrarlo
+                }
+            }
+
+            // Mostrar resultado
+            if (encontrado)
+            {
+                Console.WriteLine($"\nEl número {numero} se encontró en la posición {posicion}.");
+            }
+            else
+            {
+                Console.WriteLine($"\nEl número {numero} NO existe en el arreglo.");
+            }
         }
     }
 }
